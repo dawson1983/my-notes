9826045bc6b293e679f00569eaeb4f750a1d4576
cedric <cedricchen@gmail.com>
2025-12-17 12:13:40 -0500

    Quartz sync: Dec 17, 2025, 12:13 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://dawson1983.github.io/my-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
